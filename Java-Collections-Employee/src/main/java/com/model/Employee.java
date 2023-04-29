3017bc1b56612e65bcc9da7b3c9c446b05b75470
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-04-29 16:40:08 -0500

    new project Employee with Collections added

---
+++ Java-Collections-Employee/src/main/java/com/model/Employee.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public class Employee {
+    private long id;
+    private String name;
+
+    public Employee() {}
+
+    public Employee(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
