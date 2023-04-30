a9ddba0ed3a7bc1487ac949f0aa2460ef056908a
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-04-30 09:09:51 -0500

    updated MainMethod EmployeeDao

--- Java-Collections-Employee/src/main/java/com/dao/EmployeeDao.java
+++ Java-Collections-Employee/src/main/java/com/dao/EmployeeDao.java
@@ -10,12 +10,14 @@ import java.util.List;
 public class EmployeeDao {
     EmployeeList employeeList = new EmployeeList();
     EmployeeView employeeView = new EmployeeView();
+    List<Employee> employees;
 
     public EmployeeDao(List<Employee> employees) {
+        this.employees = employees;
         employeeList.setEmployees(employees);
     }
 
-    public void display(List<Employee> employees) {
+    public void display() {
         System.out.printf("---------------------%n");
         System.out.printf("| %4s | %-10s |%n", "ID", "NAME");
         System.out.printf("---------------------%n");
@@ -25,9 +27,20 @@ public class EmployeeDao {
         System.out.printf("---------------------%n");
     }
 
-    public void insert() {}
+    public void insert(Employee employee, long id, String name) {
+        employee = new Employee(id, name);
+        employees.add(employee);
+    }
 
-    public void delete() {}
+    public void delete(long id) {
+        for (Employee employee : employees) {
+            if(employee.getId() == id) {
+                employees.remove(id);
+                break;
+            }
+            System.out.println("EMPLOYEE ID NOT FOUND");
+        }
+    }
 
     public void update() {}
 }
--- Java-Collections-Employee/src/main/java/com/main/MainMethod.java
+++ Java-Collections-Employee/src/main/java/com/main/MainMethod.java
@@ -7,6 +7,7 @@ import com.view.EmployeeView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainMethod {
     public void defaultList(List<Employee> employees) {
@@ -18,6 +19,7 @@ public class MainMethod {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         EmployeeList employeeList = new EmployeeList();
         List<Employee> employees = new ArrayList<>();
         EmployeeDao employeeDao = new EmployeeDao(employees);
@@ -31,12 +33,28 @@ public class MainMethod {
         employees.add(employee1);
 
         employeeList.setEmployees(employees);
+        long id;
 
-
-        int choice = 1;
+        int choice = 2;
         switch (choice) {
             case 1:
-                employeeDao.display(employees);
+                employeeDao.display();
+                break;
+            case 2:
+                System.out.println("Enter employee id");
+                id = sc.nextInt();
+                System.out.println("Enter employee name");
+                String name = sc.next();
+                employeeDao.insert(employee, id, name);
+                employeeDao.display();
+                break;
+            case 3:
+                System.out.println("Enter id y want to delete");
+                id = sc.nextInt();
+                employeeDao.delete(id);
+
+//                int id = sc.nextInt();
+                employeeDao.display();
                 break;
             default:
                 System.out.println("DEFAULT CASE");
