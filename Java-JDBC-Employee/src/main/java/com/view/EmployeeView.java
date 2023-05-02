15cdf2a71c2bdfacb9048c2ed3335ec7fcaf1100
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-01 20:50:45 -0500

    EmployeeView code added

--- Java-JDBC-Employee/src/main/java/com/view/EmployeeView.java
+++ Java-JDBC-Employee/src/main/java/com/view/EmployeeView.java
@@ -1,4 +1,9 @@
 package com.view;
 
+import com.beanModel.Employee;
+
 public class EmployeeView {
+    public void printEmployeeDetails(Employee employee) {
+        System.out.printf("| %4d | %10s | %10s | %3d | %10s |", employee.getE_Id(), employee.getE_FName(), employee.getE_LName(), employee.getE_Age(), employee.getE_Post());
+    }
 }
